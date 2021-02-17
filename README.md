5fabbbecda35db0d131d60e0a4d38073d6f2aeda
Sobek <sobekcore@gmail.com>
2021-02-17 03:12:40 +0100

    Information box added

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # RGB to Hex
 RGB to Hex is a simple little program made with Go. The GUI is made with Fyne.io. The program takes 3 values from user (red, green, blue) and turns it into hex color value. It was created for easier front-end development without need to google it all the time. Super lightweight and small.
 
-## How to use?
+## How to run?
 Simply take out the shortcut file in the main directory wherever you want and use it! If you'd like to change something locally run:
 ~~~
 go build -ldflags="-H windowsgui" -o ./cmd/hex-to-rgb.exe
@@ -10,6 +10,9 @@ and to create production build run:
 ~~~
 fyne install -icon ./assets/icon.png
 ~~~
+
+## Usage
+To use simply pass right values, for RGB pass value from 0-255 for each of the colors. For Hex pass any code from #000000 to #FFFFFF. If the values vary from the range, the app will exit without returning converted color. If the values are right, the converted color will be shown with the ability to copy it and close app with one button, its that simple and that convenient.
 
 ## Contact and contribution
 If you'd like to contribute in any way, feel free to! There are probably a lot of usefull features and fixes to add. Also if you have and question hit me up anywhere, i will respond as fast as i can.
--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"fyne.io/fyne/v2/canvas"
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/data/validation"
+	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/layout"
 	"fyne.io/fyne/v2/theme"
 	"fyne.io/fyne/v2/widget"
@@ -34,25 +35,43 @@ func main() {
 	inputB := &widget.Entry{Validator: validation.NewRegexp(filter, "Must contain a number!")}
 	inputB.SetPlaceHolder("Blue (0-255)")
 
+	inputHex := &widget.Entry{Validator: validation.NewRegexp(`^#(?:[0-9a-fA-F]{6})$`, "Must be Hex!")}
+	inputHex.SetPlaceHolder("#000000")
+
 	title := widget.NewLabel("RGB to Hex")
+	titleAlt := widget.NewLabel("Hex to RGB                     ") // White-space (21 spaces) to create title offset
 	footer := widget.NewLabel("Made By Sobek")
+	footerInfo := widget.NewButtonWithIcon("", theme.InfoIcon(), func() {
+		dialog.ShowInformation("About", "RGB to Hex is a simple app\n to convert your given color\n between RGB and Hex. It's core\n is made with Go, and the\n GUI is made with Fyne.io.\n\nAuthor: Sobek", w)
+	})
 
-	hex := widget.NewEntry()
-	hex.TextStyle = fyne.TextStyle{Bold: true}
+	hexString := widget.NewEntry()
+	hexString.TextStyle = fyne.TextStyle{Bold: true}
 
-	rgb := container.NewVBox(inputR, inputG, inputB, widget.NewButton("Convert", func() {
+	rgbRed := widget.NewEntry()
+	rgbGreen := widget.NewEntry()
+	rgbBlue := widget.NewEntry()
+	rgbRed.TextStyle = fyne.TextStyle{Bold: true}
+	rgbGreen.TextStyle = fyne.TextStyle{Bold: true}
+	rgbBlue.TextStyle = fyne.TextStyle{Bold: true}
+
+	// RGB Fields calculation logic
+	rgbFields := container.NewVBox(inputR, inputG, inputB, widget.NewButton("Convert", func() {
 		c1, err1 := strconv.ParseInt(inputR.Text, 10, 16)
-		if err1 != nil {
+		if err1 != nil || c1 > 255 || c1 < 0 {
+			w.Close()
 		}
 		c2, err2 := strconv.ParseInt(inputG.Text, 10, 16)
-		if err2 != nil {
+		if err2 != nil || c2 > 255 || c2 < 0 {
+			w.Close()
 		}
 		c3, err3 := strconv.ParseInt(inputB.Text, 10, 16)
-		if err3 != nil {
+		if err3 != nil || c3 > 255 || c3 < 0 {
+			w.Close()
 		}
 		r, g, b := c1, c2, c3
 		var str = fmt.Sprintf("#%02X%02X%02X", r, g, b)
-		hex.SetText(str)
+		hexString.SetText(str)
 
 		line := &canvas.Rectangle{StrokeColor: color.NRGBA{uint8(r), uint8(g), uint8(b), 255}, StrokeWidth: 4}
 		canvas.Refresh(line)
@@ -65,25 +84,93 @@ func main() {
 				w.Clipboard().SetContent(str)
 				w.Close()
 			}),
-			fyne.NewContainerWithLayout(layout.NewVBoxLayout(), hex),
+			fyne.NewContainerWithLayout(layout.NewVBoxLayout(), hexString),
 			fyne.NewContainerWithLayout(layout.NewVBoxLayout(), line),
 			layout.NewSpacer(),
-			fyne.NewContainerWithLayout(layout.NewCenterLayout(), footer),
+			fyne.NewContainerWithLayout(layout.NewHBoxLayout(), layout.NewSpacer(),
+				footer, layout.NewSpacer(), footerInfo),
 		)
 
 		w.SetContent(contentFinal)
 	}))
 
-	content := fyne.NewContainerWithLayout(
+	// Hex Fields calculation logic
+	hexFields := container.NewVBox(inputHex, widget.NewButton("Convert", func() {
+		r := inputHex.Text[1:3]
+		g := inputHex.Text[3:5]
+		b := inputHex.Text[5:7]
+
+		c1, err1 := strconv.ParseInt(r, 16, 10)
+		if err1 != nil || len(inputHex.Text) != 7 {
+			w.Close()
+		}
+		c2, err2 := strconv.ParseInt(g, 16, 10)
+		if err2 != nil || len(inputHex.Text) != 7 {
+			w.Close()
+		}
+		c3, err3 := strconv.ParseInt(b, 16, 10)
+		if err3 != nil || len(inputHex.Text) != 7 {
+			w.Close()
+		}
+
+		var rgb1 = fmt.Sprintf("%v", c1)
+		var rgb2 = fmt.Sprintf("%v", c2)
+		var rgb3 = fmt.Sprintf("%v", c3)
+		rgbRed.SetText("R: " + rgb1)
+		rgbGreen.SetText("G: " + rgb2)
+		rgbBlue.SetText("B: " + rgb3)
+
+		line := &canvas.Rectangle{StrokeColor: color.NRGBA{uint8(c1), uint8(c2), uint8(c3), 255}, StrokeWidth: 4}
+		canvas.Refresh(line)
+
+		contentFinal := fyne.NewContainerWithLayout(
+			layout.NewVBoxLayout(),
+			fyne.NewContainerWithLayout(layout.NewCenterLayout(), titleAlt),
+			layout.NewSpacer(),
+			widget.NewButtonWithIcon("Copy to clipboard", theme.ContentCopyIcon(), func() {
+				clipboard := fmt.Sprintf("rgb(%v, %v, %v)", rgb1, rgb2, rgb3)
+				w.Clipboard().SetContent(clipboard)
+				w.Close()
+			}),
+			fyne.NewContainerWithLayout(layout.NewVBoxLayout(), rgbRed, rgbGreen, rgbBlue),
+			fyne.NewContainerWithLayout(layout.NewVBoxLayout(), line),
+			layout.NewSpacer(),
+			fyne.NewContainerWithLayout(layout.NewHBoxLayout(), layout.NewSpacer(),
+				footer, layout.NewSpacer(), footerInfo),
+		)
+
+		w.SetContent(contentFinal)
+	}))
+
+	// Hex to RGB page render
+	contentHex := fyne.NewContainerWithLayout(
 		layout.NewVBoxLayout(),
-		fyne.NewContainerWithLayout(layout.NewCenterLayout(), title),
+		fyne.NewContainerWithLayout(layout.NewCenterLayout(), titleAlt),
 		layout.NewSpacer(),
-		fyne.NewContainerWithLayout(layout.NewVBoxLayout(), rgb),
+		fyne.NewContainerWithLayout(layout.NewVBoxLayout(), hexFields),
 		layout.NewSpacer(),
-		fyne.NewContainerWithLayout(layout.NewCenterLayout(), footer),
+		fyne.NewContainerWithLayout(layout.NewHBoxLayout(), layout.NewSpacer(),
+			footer, layout.NewSpacer(), footerInfo),
 	)
 
-	w.SetContent(content)
+	changeHex := widget.NewButtonWithIcon("Hex", theme.ColorPaletteIcon(), func() {
+		w.SetContent(contentHex)
+	})
+
+	// RGB to Hex page render
+	contentRGB := fyne.NewContainerWithLayout(
+		layout.NewVBoxLayout(),
+		fyne.NewContainerWithLayout(layout.NewHBoxLayout(), layout.NewSpacer(),
+			title, layout.NewSpacer(), changeHex),
+		layout.NewSpacer(),
+		fyne.NewContainerWithLayout(layout.NewVBoxLayout(), rgbFields),
+		layout.NewSpacer(),
+		fyne.NewContainerWithLayout(layout.NewHBoxLayout(), layout.NewSpacer(),
+			footer, layout.NewSpacer(), footerInfo),
+	)
+
+	// Final rendering and app running
+	w.SetContent(contentRGB)
 	w.CenterOnScreen()
 	w.ShowAndRun()
 }
